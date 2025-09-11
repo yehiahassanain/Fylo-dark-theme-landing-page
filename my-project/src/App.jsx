aa91e836858acdf04b49e0eb1ef0bfd689c43356
yehiahassanain <89047698+yehiahassanain@users.noreply.github.com>
2025-09-11 09:23:42 +0300

    Setup Project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-red">Hello world!fvg</h1>
+    </div>
+  );
+};
+
+export default App;
